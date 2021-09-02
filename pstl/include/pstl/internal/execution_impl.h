7c60524727bd73a957c450faf38576daf990bb10
Ruslan Arutyunyan <ruslan.arutyunyan@intel.com>
2021-09-02 15:17:38 +0300

    [pstl][tag_dispatch] Remove unnecessary traits (#10)

--- pstl/include/pstl/internal/execution_impl.h
+++ pstl/include/pstl/internal/execution_impl.h
@@ -40,76 +40,6 @@ struct __is_random_access_iterator : decltype(__is_iterator_of<std::random_acces
 {
 };
 
-template <typename... _IteratorTypes>
-struct __is_forward_iterator : decltype(__is_iterator_of<std::forward_iterator_tag, _IteratorTypes...>(0))
-{
-};
-
-template <typename Policy>
-struct __policy_traits
-{
-};
-
-template <>
-struct __policy_traits<sequenced_policy>
-{
-    typedef std::false_type __allow_parallel;
-    typedef std::false_type __allow_unsequenced;
-    typedef std::false_type __allow_vector;
-};
-
-template <>
-struct __policy_traits<unsequenced_policy>
-{
-    typedef std::false_type __allow_parallel;
-    typedef std::true_type __allow_unsequenced;
-    typedef std::true_type __allow_vector;
-};
-
-template <>
-struct __policy_traits<parallel_policy>
-{
-    typedef std::true_type __allow_parallel;
-    typedef std::false_type __allow_unsequenced;
-    typedef std::false_type __allow_vector;
-};
-
-template <>
-struct __policy_traits<parallel_unsequenced_policy>
-{
-    typedef std::true_type __allow_parallel;
-    typedef std::true_type __allow_unsequenced;
-    typedef std::true_type __allow_vector;
-};
-
-template <typename _ExecutionPolicy>
-using __allow_vector =
-    typename __internal::__policy_traits<typename std::decay<_ExecutionPolicy>::type>::__allow_vector;
-
-template <typename _ExecutionPolicy>
-using __allow_unsequenced =
-    typename __internal::__policy_traits<typename std::decay<_ExecutionPolicy>::type>::__allow_unsequenced;
-
-template <typename _ExecutionPolicy>
-using __allow_parallel =
-    typename __internal::__policy_traits<typename std::decay<_ExecutionPolicy>::type>::__allow_parallel;
-
-template <typename _ExecutionPolicy, typename... _IteratorTypes>
-typename std::conjunction<__allow_vector<_ExecutionPolicy>,
-                          __is_random_access_iterator<_IteratorTypes>...>::type
-__is_vectorization_preferred(_ExecutionPolicy&&)
-{
-    return {};
-}
-
-template <typename _ExecutionPolicy, typename... _IteratorTypes>
-typename std::conjunction<__allow_parallel<_ExecutionPolicy>,
-                          __is_random_access_iterator<_IteratorTypes>...>::type
-__is_parallelization_preferred(_ExecutionPolicy&&)
-{
-    return {};
-}
-
 struct __serial_backend
 {
 };
@@ -141,20 +71,9 @@ struct __parallel_tag
     using __backend_tag = __par_backend_tag;
 };
 
-struct __parallel_forward_tag
-{
-    using __is_vector = std::false_type;
-    // backend tag can be change depending on
-    // TBB availability in the environment
-    using __backend_tag = __par_backend_tag;
-};
-
 template <class _IsVector, class... _IteratorTypes>
-using __tag_type =
-    typename std::conditional<__internal::__is_random_access_iterator<_IteratorTypes...>::value,
-                              __parallel_tag<_IsVector>,
-                              typename std::conditional<__is_forward_iterator<_IteratorTypes...>::value,
-                                                        __parallel_forward_tag, __serial_tag<_IsVector>>::type>::type;
+using __tag_type = typename std::conditional<__internal::__is_random_access_iterator<_IteratorTypes...>::value,
+                                             __parallel_tag<_IsVector>, __serial_tag<_IsVector>>::type;
 
 template <class... _IteratorTypes>
 __serial_tag</*_IsVector = */ std::false_type>
